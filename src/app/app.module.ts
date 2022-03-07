3ce6fd89a6d732b8b50d40b7008ccde0895d3592
Rafa Garrido <rafagarridolara@gmail.com>
2022-03-06 23:44:35 -0400

    Add events list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from 'src/app/events/events-list.component';
+import { EventsThumbnailsComponent } from 'src/app/events/events-thumbnail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EventsListComponent, EventsThumbnailsComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
